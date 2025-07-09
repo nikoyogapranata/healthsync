85d8519eb4da230ce36c845e430d90927d9b8390
nikoyogapranata <nikoyogapranata@gmail.com>
2025-07-09 22:37:55 +0700

    adding doctor dashboard

--- app/doctor-dashboard/page.tsx
+++ app/doctor-dashboard/page.tsx
@@ -2,8 +2,8 @@ import { DashboardDoctor } from "@/components/dashboard-doctor"
 
 export default function DoctorDashboardPage() {
   return (
-    <main className="min-h-screen bg-[#f9fafb] pb-12">
-      <div className="container mx-auto px-4 py-8">
+    <main className="min-h-screen w-full bg-[#f9fafb]">
+      <div className="w-full h-full">
         <DashboardDoctor />
       </div>
     </main>
